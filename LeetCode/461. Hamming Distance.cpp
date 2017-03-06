61f6b5119ba599bea84c0ced8650950b7b42ee32
Sindyang <863326066@qq.com>
2017-03-06 10:58:04 +0800

    second commit

--- LeetCode/461. Hamming Distance.cpp
+++ LeetCode/461. Hamming Distance.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
 	int hammingDistance(int x, int y) {
 		int count = 0;
-		while (x || y) {
-			//x的最低位和y的最低位不相同
-			if ((x & 1) ^ (y & 1)) {
-				count++;
-			}
-			x >>= 1;
-			y >>= 1;
+		int temp = x^y;
+		while (temp) {
+			count += temp % 2;
+			temp >>= 1;
 		}
 		return count;
 	}
-
 };
