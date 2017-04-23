29aa4b271f4bca0db74a1bcc7b3bed92276d0dda
Sindyang <863326066@qq.com>
2017-04-23 15:47:19 +0800

    first commit

---
+++ LeetCode/142. Linked List Cycle II.cpp
@@ -0,0 +1,27 @@
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};
+class Solution {
+public:
+	ListNode *detectCycle(ListNode *head) {
+		ListNode *fast = head;
+		ListNode *slow = head;
+		while ((fast != NULL) && (slow != NULL) && (fast->next != NULL)) {
+			fast = fast->next->next;
+			slow = slow->next;
+			if (fast == slow) {
+				fast = head;
+				while (fast != slow) {
+					fast = fast->next;
+					slow = slow->next;
+				}
+				return fast;
+			}
+		}
+		return NULL;
+	}
+};
