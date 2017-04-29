cbb487eded149c0b589e6541e124ab10bee32a0f
Sindyang <863326066@qq.com>
2017-04-29 19:17:33 +0800

    Create 88. Merge Sorted Array.cpp

---
+++ LeetCode/88. Merge Sorted Array.cpp
@@ -0,0 +1,31 @@
+//solution 1
+class Solution {
+public:
+	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+		vector<int> temp;
+		int i = 0;
+		int j = 0;
+		sort(nums1.begin(), nums1.begin() + m);
+		sort(nums2.begin(), nums2.begin() + n);
+		while (i < m && j < n) {
+			if (nums1[i] < nums2[j]) {
+				temp.push_back(nums1[i++]);
+			} else if (nums1[i] > nums2[j]) {
+				temp.push_back(nums2[j++]);
+			} else {
+				temp.push_back(nums1[i++]);
+				temp.push_back(nums2[j++]);
+			}
+		}
+		while (i < m) {
+			temp.push_back(nums1[i++]);
+		}
+		while (j < n) {
+			temp.push_back(nums2[j++]);
+		}
+		nums1.clear();
+		for (i = 0; i < (signed) temp.size(); i++) {
+			nums1.push_back(temp[i]);
+		}
+	}
+};
