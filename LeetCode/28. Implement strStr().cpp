87a50371f02da5cf7a2162dffe6afecd721f6fb9
Sindyang <863326066@qq.com>
2017-04-30 18:07:54 +0800

    Update 28. Implement strStr().cpp

--- LeetCode/28. Implement strStr().cpp
+++ LeetCode/28. Implement strStr().cpp
@@ -4,30 +4,25 @@ public:
 	int strStr(string haystack, string needle) {
 		int i = 0;
 		int j = 0;
-		int pos = 0;
 		if (needle.empty()) {
 			return 0;
 		}
 		if (haystack.length() < needle.length()) {
 			return -1;
 		}
-		while (pos < (signed) haystack.length() - (signed) needle.length() + 1) {
-			if (haystack[pos] != needle[j]) {
-				pos++;
-				j = 0;
-			}
-			i = pos;
-			while (haystack[i] == needle[j] && j < (signed) needle.length()) {
+		while (i < (signed) haystack.length() && j < (signed) needle.length()) {
+			if (haystack[i] == needle[j]) {
 				i++;
 				j++;
-			}
-			if (j == (signed) needle.length()) {
-				return i - j;
 			} else {
+				i = i - j + 1;
 				j = 0;
-				pos++;
 			}
 		}
-		return -1;
+		if (j == (signed) needle.length()) {
+			return i - j;
+		} else {
+			return -1;
+		}
 	}
 };
